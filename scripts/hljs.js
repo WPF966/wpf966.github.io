d599408156c2e4791b9bb8bfc1e35b1f6e655ca2
GitHub WPF966 <GitHub 2447293966@qq.com>
2021-08-16 16:50:31 +0800

    Site updated: 2021-08-16 16:50:31

--- scripts/hljs.js
+++
@@ -1,9 +0,0 @@
-if (ds.line_number === 'frontend') {
-  console.log("show line number in front-end");
-  var lines = texts.length - 1;
-  var $numbering = $('<ul/>').addClass('pre-numbering');
-  $(this).addClass('has-numbering').parent().append($numbering);
-  for (i = 1; i <= lines; i++) {
-    $numbering.append($('<li/>').text(i));
-  }
-}
